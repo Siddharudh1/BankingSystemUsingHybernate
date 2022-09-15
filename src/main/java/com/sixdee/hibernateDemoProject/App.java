832e6f8a323d716119dcca2449b46fce1099a0d1
Siddharudh1 <110817247+Siddharudh1@users.noreply.github.com>
2022-09-15 18:40:20 +0530

    Update App.java

--- src/main/java/com/sixdee/hibernateDemoProject/App.java
+++ src/main/java/com/sixdee/hibernateDemoProject/App.java
@@ -13,16 +13,37 @@ public class App
 {
     public static void main( String[] args )
     {
-       Customer c = new Customer();
-       c.setCid(100);
-       c.setCname("SIDDU");
-       c.setAddress("BIDAR");
+       //Storing the data in the database....
+      /* Customer c = new Customer();
+       c.setCustomerid(102);
+       c.setFirstname("Sachin");
+       c.setUsername("Sachi");
+       c.setLastname("methre");
+       c.setAge(22);
+       c.setAddress("Bidar");
+       c.setMobileNumber("7022104694");
+       c.setDateOfBirth("01/06/1999");
+       c.setBalance(2000);
        
        Configuration con = new Configuration().configure().addAnnotatedClass(Customer.class);
-       SessionFactory sf =con.buildSessionFactory();
+       ServiceRegistry reg= new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+       SessionFactory sf =con.buildSessionFactory(reg);
        Session session = sf.openSession();
        Transaction tx = session.beginTransaction();
        session.save(c);
-       tx.commit();
+       tx.commit();*/
+    	
+    	//Feteching the data in the console.......
+    	
+    	Customer c = null;
+    	Configuration con = new Configuration().configure().addAnnotatedClass(Customer.class);
+        ServiceRegistry reg= new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+        SessionFactory sf =con.buildSessionFactory(reg);
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        //c =(Customer) session.get(Customer.class, 101);
+        c =(Customer) session.get(Customer.class, 102);
+        tx.commit();
+        System.out.println(c);
     }
 }

329637ad3ef91f8eaa854b1cdc1ef5baeabdc5b1
Siddharudh1 <110817247+Siddharudh1@users.noreply.github.com>
2022-09-15 18:41:23 +0530

    Update Customer.java

--- src/main/java/com/sixdee/hibernateDemoProject/Customer.java
+++ src/main/java/com/sixdee/hibernateDemoProject/Customer.java
@@ -6,25 +6,73 @@ import javax.persistence.Id;
 @Entity
 public class Customer {
 	@Id
-	private int cid;
-	private String cname;
+	private int customerid;
+	private String username;
+	private String firstname;
+	private String lastname;
+	private int age;
 	private String address;
-	public int getCid() {
-		return cid;
+	private String mobileNumber;
+	private String dateOfBirth;
+	private int balance;
+	public int getBalance() {
+		return balance;
 	}
-	public void setCid(int cid) {
-		this.cid = cid;
+	public void setBalance(int balance) {
+		this.balance = balance;
 	}
-	public String getCname() {
-		return cname;
+	public int getCustomerid() {
+		return customerid;
 	}
-	public void setCname(String cname) {
-		this.cname = cname;
+	public void setCustomerid(int customerid) {
+		this.customerid = customerid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
 	}
 	public String getAddress() {
 		return address;
 	}
 	public void setAddress(String address) {
 		this.address = address;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	@Override
+	public String toString() {
+		return "Customer [customerid=" + customerid + ", username=" + username + ", firstname=" + firstname
+				+ ", lastname=" + lastname + ", age=" + age + ", address=" + address + ", mobileNumber=" + mobileNumber
+				+ ", dateOfBirth=" + dateOfBirth + ", balance=" + balance + "]";
 	}
 }
